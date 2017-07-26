b256dad8c9e7871c82955f24690fd5bcd93a5eea
Somin Wadhwa <sominwadhwa@users.noreply.github.com>
2017-07-26 23:10:43 +0530

    Adding requirements.txt

---
+++ test/init.py
@@ -0,0 +1,13 @@
+import sklearn
+import pubchempy
+import matplotlib
+import seaborn
+import pandas
+import numpy
+import scipy
+import collections
+import pickle
+import time
+import math
+
+print ("Imports successful. Good to go!")
